10d7b484c77de068f713f76783bad5198d2e9f87
alvi2496 <alvi.utsab@gmail.com>
2020-06-18 01:56:33 -0700

    api for classifying discussions

--- src/api.py
+++ src/api.py
@@ -1,21 +1,36 @@
 from flask import Flask, request, jsonify
 from WordEmbedder import WordEmbedder
+from SimilarWordInjector import SimilarWordInjector
+from Vectorizer import Vectorizer
 from Classifier import Classifier
+from Utils import TextProcessor
+from pathlib import Path
+import nltk
 
 app = Flask(__name__)
+ROOT_DIR = str(Path(__file__).parent.parent)
 
-
-@app.route('/', methods = ['GET'])
-def home():
-    return "<h1>Hello world</h1>"
-
-@app.route('/books', methods=['GET'])
-def books(): 
-    return jsonify(Book().index())
+Injector = SimilarWordInjector()
+Vectorizer = Vectorizer()
+Classifier = Classifier()
+nltk.download('wordnet')
 
 @app.route('/classify', methods=['POST'])
 def classify():
-    return jsonify(Classifier().classify(request.json))
+    return jsonify(
+        {
+            'label': 
+            Classifier.classify(
+                Vectorizer.text_to_vector(
+                    Injector.inject(
+                        TextProcessor().process(
+                            request.json['text']
+                        )
+                    )
+                )
+            )
+        }
+    )
 
 @app.route('/we', methods=['GET'])
 def we():
--- src/config.py
+++ src/config.py
@@ -1,5 +1,5 @@
 from dotenv import load_dotenv
 from pathlib import Path
 
-env_path = Path(__file__).parent.parent / '.env'
+env_path = Path(__file__).parent.parent + '/.env'
 load_dotenv(dotenv_path=env_path)
