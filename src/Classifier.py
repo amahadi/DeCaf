2515cef87771676df951418d96ba574a205eb9f6
alvi2496 <alvi.utsab@gmail.com>
2020-06-18 01:55:19 -0700

    complete classifier for api

--- src/Classifier.py
+++ src/Classifier.py
@@ -1,12 +1,19 @@
+from joblib import load
+from pathlib import Path
+import warnings
+
 class Classifier:
     
     def __init__(self):
+        warnings.simplefilter("ignore")
+        ROOT_DIR = str(Path(__file__).parent.parent)
+        print("Loading Classifier Model")
+        self.model = load(ROOT_DIR + "/models/lsvm.joblib") 
         print('Classifier ready!!')
         
     def classify(self, data):
-        res = {
-            'status': 'Processed',
-            'action': 'lowercased',
-            'text': data['text'].lower()
-        }
-        return res
+        label = self.model.predict(data)[0]
+        if label == 1:
+            return 'design'
+        else:
+            return 'general'
